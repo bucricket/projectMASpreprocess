142dc7c21aa3fa0e39b988bf997c2540a98e749e
bucricket <bucricket@gmail.com>
2017-02-07 14:37:21 -0500

    Changed base folder to $PREFIX

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ out = p.communicate()
 condaPath = out[0][:-1]
 libEnv = os.path.join(condaPath,'lib')    
 
-processDir = base
+processDir = '$PREFIX'
 libDir = os.path.join(processDir,'source','lib')
 
 
